0c94e50d47747cb39adf1730263292bb9b0457b0
18940 <18940646130@163.com>
2020-07-07 08:03:34 +0800

    自定义校验

--- api/views.py
+++ api/views.py
@@ -3,14 +3,16 @@ import re
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated  # 导入权限模块
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication  # 导入JWT模块
+
+from api.authentication import JWTAuthentication  #导入自定义校验模块
 from api.serializers import UserModelSerializer  #导入反序列化器
 from utils.response import APIResponse
 
 # 获取token 两种url
 class UserAPIV(APIView):
     permission_classes = [IsAuthenticated]  # 权限：只允许认证通过的用户访问   游客无权访问
-
-    authentication_classes = [JSONWebTokenAuthentication]
+    authentication_classes = [JWTAuthentication]   # 自定义校验模块
+    # authentication_classes = [JSONWebTokenAuthentication]     # 系统校验模块
 
     def get(self, request, *args, **kwargs):
 
